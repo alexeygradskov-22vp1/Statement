295bb9fb6284faf914b02b4c850c41f4b3172300
Алексей <gradskovaleksej@gmail.com>
2024-12-25 22:45:35 +0300

    Определена логика сервиса

--- src/main/java/ru/gav/creditbank/statement/controller/StatementController.java
+++ src/main/java/ru/gav/creditbank/statement/controller/StatementController.java
@@ -6,17 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.gav.__model_.StatementsApi;
 import ru.gav.__model_.model.LoanOfferDto;
 import ru.gav.__model_.model.LoanStatementRequestDto;
+import ru.gav.creditbank.statement.service.StatementService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class StatementController implements StatementsApi {
+    private final StatementService statementService;
     @Override
     public ResponseEntity<Void> choosingOffer(LoanOfferDto loanOfferDto) {
-        return null;
+        statementService.selectOffer(loanOfferDto);
+        return ResponseEntity.ok().build();
     }
 
     @Override
-    public ResponseEntity<Void> firstValidationApplication(LoanStatementRequestDto loanStatementRequestDto) {
-        return null;
+    public ResponseEntity<List<LoanOfferDto>> firstValidationApplication(LoanStatementRequestDto loanStatementRequestDto) {
+        return ResponseEntity.ok(statementService.score(loanStatementRequestDto));
     }
 }
